c92a78c0712a27a6612b70ad4eff9b2a84ef6fe4
prneet <69181346+prneet@users.noreply.github.com>
2020-09-03 12:19:14 +0900

    active_storage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,10 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 
 end
