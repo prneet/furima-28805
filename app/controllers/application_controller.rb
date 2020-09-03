384831c7b5a9e6e635064404aab39a5284943bfe
prneet <69181346+prneet@users.noreply.github.com>
2020-09-03 17:20:16 +0900

    新規登録ビューファイル編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :encrypted_password])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
 end
