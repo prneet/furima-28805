af46a6daac11db1eaf06e2df0de51c150c4b72c1
prneet <69181346+prneet@users.noreply.github.com>
2020-09-03 19:16:00 +0900

    userモデルのヴァリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: '英数字６文字以上を使用してください。' }
+  
+  with_options presence: true do
+    validates :name
+    validates :birthday
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用して下さい' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用して下さい' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
