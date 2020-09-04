c4e784f63ab39ba65355f3f1980700a7b2e8f846
prneet <69181346+prneet@users.noreply.github.com>
2020-09-04 19:09:22 +0900

    postageのactivehash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
   belongs_to_active_hash :status
+  belongs_to_active_hash :postage
+  
   has_one_attached :image
   has_one :order
 end
