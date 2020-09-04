2b194ee36c897de8a8fc6949488570b4892348da
prneet <69181346+prneet@users.noreply.github.com>
2020-09-04 19:12:53 +0900

    都道府県のactivehash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :genre
   belongs_to_active_hash :status
   belongs_to_active_hash :postage
-  
+  belongs_to_active_hash :region
   has_one_attached :image
   has_one :order
 end
