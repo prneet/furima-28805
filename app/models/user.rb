6b8e847e5b516d4d14f6c20bc6d714fbfc52f04b
prneet <69181346+prneet@users.noreply.github.com>
2020-09-04 18:14:50 +0900

    userモデル正規表現変数化/userモデルテストコード追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  ZENKAKU_KANA_REGEX = /\A[ァ-ン]+\z/.freeze
   validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: '英数字６文字以上を使用してください。' }
   
   with_options presence: true do
     validates :name
     validates :birthday
   end
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用して下さい' } do
+  with_options presence: true, format: { with: ZENKAKU_REGEX, message: '全角文字を使用して下さい' } do
     validates :first_name
     validates :last_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用して下さい' } do
+  with_options presence: true, format: { with: ZENKAKU_KANA_REGEX, message: '全角カタカナを使用して下さい' } do
     validates :first_name_kana
     validates :last_name_kana
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,24 +31,38 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
     end
-
     context 'ユーザー登録ができない時' do
       it "nameが空では登録できないこと" do
         @user.name = nil
         @user.valid?
         expect(@user.errors.full_messages).to include("Name can't be blank")
       end
-
       it "emailが空では登録できないこと" do
         @user.email = nil
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
-
+      it "メールアドレスが既に登録されているものは登録できないこと" do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it "メールアドレスに@が含まれないと登録できないこと" do
+        @user.email = "aaaaaaaa.gmail.com"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it "passwordが空では登録できないこと" do
         @user.password = nil
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it "passwordが英数字混合でないと登録できないこと" do
+        @user.password = "aaaaaa"
+        @user.password_confirmation = "aaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password 英数字６文字以上を使用してください。")
       end
       it "passwordが存在してもpassword_confirmationが空では登録できないこと" do
         @user.password_confirmation = ""
