60a1fb8049270eb5b5d2e9d4706ac65bea1ae68a
prneet <69181346+prneet@users.noreply.github.com>
2020-09-07 11:25:46 +0900

    showファイル商品情報の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:show]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,7 +21,7 @@ class ItemsController < ApplicationController
 
   def show
   end
-  
+
   private
 
   def item_params
@@ -31,5 +32,9 @@ class ItemsController < ApplicationController
     unless user_signed_in?
       redirect_to action: :index
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
