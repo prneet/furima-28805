fb13844bb8c6ffdce518b5e451f74ce00677ff41
prneet <69181346+prneet@users.noreply.github.com>
2020-09-08 21:15:47 +0900

    購入処理の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,13 +24,13 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_address).permit(:order_id, :postal_code, :genre_id, :city, :address, :build, :phone_number, :token).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_address).permit(:order_id, :postal_code, :region_id, :city, :address, :build, :phone_number, :token).merge(user_id: current_user.id, item_id: @item.id)
   end
 
   def pay_item
     Payjp.api_key = "sk_test_898973b7cb3626ff2ca3b596"
     Payjp::Charge.create(
-      amount: @item.id,
+      amount: @item.price,
       card: order_params[:token],
       currency:'jpy'
     )
--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,11 +1,11 @@
 class OrderAddress
 
   include ActiveModel::Model
-  attr_accessor :user_id, :order_id, :postal_code, :genre_id, :city, :address, :build, :phone_number
+  attr_accessor :user_id, :order_id, :postal_code, :region_id, :city, :address, :build, :phone_number, :token, :item_id
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :genre_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :region_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :address
     validates :phone_number, format: {with: /\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?\d{4})[-)]?)|\d{1,4}\-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/ }
