907c7a279496bb7dcd630e73208f5d626e33ce97
prneet <69181346+prneet@users.noreply.github.com>
2020-09-08 10:52:45 +0900

    ordersコントローラー仮置き

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,18 @@
 class OrdersController < ApplicationController
   def index
   end
+
+  def new
+    @order = User.new
+  end
+
+  def create
+    @donation = User.create(donation_params)
+  end
+
+  private
+
+  def donation_params
+    params.require(:user).permit(:name, :name_reading, :nickname)
+  end
 end
