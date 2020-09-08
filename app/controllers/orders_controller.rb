5c3a11ea1242e08e4fd1e73e6c931558394e1130
prneet <69181346+prneet@users.noreply.github.com>
2020-09-08 11:41:57 +0900

    order_addressフォーム作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,22 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :new, :create]
+
   def index
   end
 
   def new
-    @order = User.new
   end
 
   def create
-    @donation = User.create(donation_params)
   end
 
   private
 
-  def donation_params
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def order_params
     params.require(:user).permit(:name, :name_reading, :nickname)
   end
 end
