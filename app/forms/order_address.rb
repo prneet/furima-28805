e63d176e976860be1af375e2bb13cd7085bb6146
prneet <69181346+prneet@users.noreply.github.com>
2020-09-09 13:46:36 +0900

    order_address単体テスト

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -8,7 +8,8 @@ class OrderAddress
     validates :region_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :address
-    validates :phone_number, format: {with: /\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?\d{4})[-)]?)|\d{1,4}\-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/ }
+    validates :phone_number, format: {with: /\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?\d{4})[-)]?)|\d{1,4}\-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/, message: "is invalid"}
+    validates :token
   end
 
   def save
